36040988988ccc727c73748f750b0c84d9253f32
jesse <jsoldat@hotmail.com>
2017-03-16 15:07:52 +0700

    uploader works

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ElementRef, Input} from '@angular/core';
+import {Http, Response} from '@angular/http';
+import {FileUploader} from 'ng2-file-upload/ng2-file-upload';
+
+import "rxjs/add/operator/do";
+import "rxjs/add/operator/map";
+
+const URL = '/';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+	public uploader:FileUploader = new FileUploader({url: URL, itemAlias: 'photo'});
+
+  constructor(private http: Http, private el: ElementRef){}
+
+  ngOnInit(){
+  	this.uploader.onAfterAddingFile = (file) => {file.withCredentials = false;};
+  	this.uploader.onCompleteItem = (item:any, response:any, status:any, headers:any) => {
+  		console.log('ImageUploaded: ',item,status,response);
+  	};
+  }
+
+  upload(){
+
+  }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FileSelectDirective } from 'ng2-file-upload';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FileSelectDirective
+    
   ],
   imports: [
     BrowserModule,
