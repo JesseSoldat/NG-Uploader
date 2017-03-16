3103125431b9dcc73f79638eb034a2730e19e081
jesse <jsoldat@hotmail.com>
2017-03-16 14:04:09 +0700

    server runs

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+
+var app = express();
+var port = 3001;
+
+app.set('view engine', 'ejs');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/', routes);
+
+app.listen(port, function(){
+	console.log('Server started at: '+port);
+})
